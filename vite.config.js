702cdfac5c7a1be4379ec4f51fe49d850c39538b
Krishna Paladugu <krismohi@gmail.com>
2025-08-06 00:23:21 +0530

    Fix tailwind css issue

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: 'es2022', // Support top-level await
+    target: 'es2022',
     rollupOptions: {
       output: {
         manualChunks: undefined,
